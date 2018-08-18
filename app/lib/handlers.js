547e93cd8295f11e36f9c74fa3edf3c32ea581b2
kolbert <kolbert@yandex.ru>
2018-08-18 10:04:47 +0300

    handlers users/post

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -59,11 +59,11 @@ handlers._users.post = function (data, callback) {
                     // store user
 
                     _data.create('users', phone, userObject, function (err) {
-                        if (!err){
+                        if (!err) {
                             callback(200);
                         } else {
                             console.log(err);
-                            callback(500,{'Error': 'Could not create the new user'});
+                            callback(500, {'Error': 'Could not create the new user'});
                         }
                     })
 
@@ -89,13 +89,13 @@ handlers._users.post = function (data, callback) {
 // @TODO Only let an authenticated user access their object
 handlers._users.get = function (data, callback) {
     // check that phone number is valid
-    let phone = typeof (data.queryStringObject.phone) == 'string'&& data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
-    if(phone) {
+    let phone = typeof (data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+    if (phone) {
         _data.read('users', phone, function (err, data) {
             if (!err && data) {
                 // Remove hash before returning object
                 delete data.hashedPassword;
-                callback(200,data)
+                callback(200, data)
 
             } else {
                 callback(404);
@@ -109,8 +109,58 @@ handlers._users.get = function (data, callback) {
 };
 
 // users - put
+// Required data: phone
+// Optional data: firstName, lastName, password (at least one must be specified
+// @TODO Only let an authenticated udpaid their own object
 handlers._users.put = function (data, callback) {
+    // check for the required field
+    let phone = typeof (data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
 
+    // Check for the optional fields
+    let firstName = typeof (data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+    let lastName = typeof (data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+    let password = typeof (data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+
+    // Error if the phone is invalid
+    if (phone) {
+        // Error if nothing is sent to update
+        if (firstName || lastName || password) {
+            // lookup the user
+            _data.read('users', phone, function (err, userData) {
+                if (!err && userData) {
+                    // update the fields necessary
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.hashedPassword = helpers.hash(password);
+                    }
+
+                    // store the new updates
+                    _data.update('users', phone, userData, function (err) {
+                        if (!err) {
+                            callback(200);
+                        } else {
+                            console.log(err);
+                            callback(500, {'Error': 'Could not update the user'})
+                        }
+                    })
+
+                } else {
+                    callback(400, {'Error': 'The specified user does not exist'})
+                }
+            })
+
+        } else {
+            callback(400, {'Error': 'Missing fields to update'})
+        }
+
+    } else {
+        callback(400, {'Error': 'Missing required field'})
+    }
 
 };
 
