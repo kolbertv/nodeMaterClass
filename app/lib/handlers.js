2e25aea8948529305e8cc31fc022832a18479cc9
kolbert <kolbert@yandex.ru>
2018-08-18 21:01:29 +0300

    handlers users/delete

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -166,7 +166,32 @@ handlers._users.put = function (data, callback) {
 
 
 // users - delete
+// required field: phone
+// @TODO only for an authenticated user for delete own object
+// @TODO delete ay other data files associated with this user
 handlers._users.delete = function (data, callback) {
+    // check that phone number is valid
+    let phone = typeof (data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+    if (phone) {
+        _data.read('users', phone, function (err, data) {
+            if (!err && data) {
+
+                _data.delete('users', phone, function (err) {
+                    if(!err){
+                        callback(200);
+                    } else {
+                        callback(500, {'Error': ' Could not delete the specified user'})
+                    }
+                })
+
+            } else {
+                callback(400, {'Error': 'Could not find the specified user'});
+            }
+        });
+
+    } else {
+        callback(400, {"Error": "Missing required field"})
+    }
 
 
 };
